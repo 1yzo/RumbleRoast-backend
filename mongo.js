6ccfa601055571c3c9d2cde31c96fea33738f9b4
Osman Ali <okali@uci.edu>
2018-09-13 01:21:29 -0700

    Change database

--- mongo.js
+++ mongo.js
@@ -5,7 +5,7 @@ mongoose.Promise = global.Promise;
 
 const username = secrets.mongoUser;
 const password = secrets.mongoPass;
-const mongoUri = `mongodb://${username}:${password}@ds046267.mlab.com:46267/rumble-roast`;
+const mongoUri = `mongodb://${username}:${password}@ds028559.mlab.com:28559/rumble-roast`;
 
 const connect = () => {
     return mongoose.connect(mongoUri, { useNewUrlParser: true });
